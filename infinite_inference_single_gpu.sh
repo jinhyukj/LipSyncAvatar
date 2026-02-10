20842e39f929138acbfc7b9c7342c35f7c75a0a9
JinhyukJang <jhjangbot@kaist.ac.kr>
2026-02-10 21:19:10 +0900

    inference_ready

--- infinite_inference_single_gpu.sh
+++ infinite_inference_single_gpu.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=1
 export NCCL_DEBUG=WARN
 export NCCL_DEBUG_SUBSYS=OFF
-CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES torchrun --nproc_per_node=1 --master_port=29101  minimal_inference/s2v_streaming_interact.py \
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES torchrun --nproc_per_node=1 --master_port=29102  minimal_inference/s2v_streaming_interact.py \
      --ulysses_size 1 \
      --task s2v-14B \
      --size "704*384" \
